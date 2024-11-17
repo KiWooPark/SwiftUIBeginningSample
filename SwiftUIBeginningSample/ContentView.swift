afd05404e0b7aa33b003d012754098e7c7e160c0
박기우 <KWiOS0101@gmail.com>
2024-11-17 17:42:27 +0900

    SwiftUI 예제 프로젝트 생성

--- SwiftUIBeginningSample/ContentView.swift
+++ SwiftUIBeginningSample/ContentView.swift
@@ -14,6 +14,7 @@ struct ContentView: View {
                 .imageScale(.large)
                 .foregroundStyle(.tint)
             Text("Hello, world!")
+            Text("시작")
         }
         .padding()
     }
